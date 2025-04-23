50a5f3ec6174c267327a048406a7ebeabe3c980e
jidrizi <idrizijuxhin@gmail.com>
2025-04-23 15:12:25 +0200

    ex01 was also more of the same

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: jidrizi <jidrizi@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 14:34:40 by jidrizi           #+#    #+#             */
-/*   Updated: 2025/04/23 14:38:34 by jidrizi          ###   ########.fr       */
+/*   Updated: 2025/04/23 15:10:52 by jidrizi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,18 @@ Fixed::Fixed(const Fixed &copy)
 {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = copy;
+}
+
+Fixed::Fixed(const int value)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->fixedPointValue = value << this->fractionalBits;
+}
+
+Fixed::Fixed(const float value)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->fixedPointValue = roundf(value * (1 << this->fractionalBits));
 }
 
 Fixed	&Fixed::operator=(const Fixed &copy)
@@ -38,12 +50,28 @@ Fixed::~Fixed()
 
 int	Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
+	// std::cout << "getRawBits member function called" << std::endl;
 	return (this->fixedPointValue);
 }
 
 void	Fixed::setRawBits(int const raw)
 {
-	std::cout << "setRawBits member function called" << std::endl;
+	// std::cout << "setRawBits member function called" << std::endl;
 	this->fixedPointValue = raw;
+}
+
+float	Fixed::toFloat(void) const
+{
+	return ((float)this->fixedPointValue / (1 << this->fractionalBits));
+}
+
+int	Fixed::toInt(void) const
+{
+	return (this->fixedPointValue >> this->fractionalBits);
+}
+
+std::ostream	&operator<<(std::ostream &out, const Fixed &fixed)
+{
+	out << fixed.toFloat();
+	return (out);
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,9 +6,27 @@
 /*   By: jidrizi <jidrizi@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 14:34:53 by jidrizi           #+#    #+#             */
-/*   Updated: 2025/04/23 14:35:02 by jidrizi          ###   ########.fr       */
+/*   Updated: 2025/04/23 15:09:31 by jidrizi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
+int	main(void)
+{
+	Fixed	a;
+	Fixed	const b( 10 );
+	Fixed	const c( 42.42f );
+	Fixed	const d( b );
+
+	a = Fixed( 1234.4321f );
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	return (0);
+}
